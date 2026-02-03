572e13553ec3005fc6eb2dafb1931d134f0e3385
FabricioCosta0697 <fabricioluis16@gmail.com>
2026-02-03 20:15:04 -0300

    criação de hash

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,5 +6,8 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str = "sqlite:///./pdv.db"  # MVP local
 
+    JWT_SECRET_KEY: str = "CHANGE_ME_SUPER_SECRET"
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 8  # 8 horas
 
 settings = Settings()
